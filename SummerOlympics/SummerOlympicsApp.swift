f5023bf75954317269ff8e8127f854de26c20c5a
Vincent Torres <evanwater77@gmail.com>
2026-04-20 11:26:45 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by TORRES, VINCENT on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
